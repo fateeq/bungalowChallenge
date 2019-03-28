1c1be45dc2a97ad6e873fac64ee6814a9d1330a8
fateeq <44147888+fateeq@users.noreply.github.com>
2019-03-28 14:13:04 -0400

    moved data to main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		id: 16,
+		image: 'test.jpg',
+		roomPrice: 916,
+		headline: 'Historic Home in Queen Anne',
+		roomAvailability: 1,
+		totalRoomCount: 5,
+		earliestAvailable: '2019-03-28',
+	}
+})
