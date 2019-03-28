58f30637872b259ca1ac19403dcfac9b41c0286d
fateeq <44147888+fateeq@users.noreply.github.com>
2019-03-28 16:48:46 -0400

    changed results index # in card component from static to dynamic

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 Vue.component('card', {
 	props: {
-		results: []
+		results: [],
+		index: ''
 	},
 	template: `
 		<div id="card">
-        <img :src="results[0].images[0].sm_url"/>
+        <img :src="results[index].images[0].sm_url"/>
         <div class="listing-details">
-            <div class="room-price">$ {{ results[0].room_prices[0] }}</div>
-            <h3 class="headline"> {{ results[0].headline }} </h3>
-            <h6 class="room-availability">{{ results[0].available_room_count }} of {{ results[0].total_room_count }} rooms available</h6>
-            <h6 class="date-available">Earliest availabe: {{ results[0].earliest_available_date }}</h6>
+            <div class="room-price">$ {{ results[index].room_prices[0] }}</div>
+            <h3 class="headline"> {{ results[index].headline }} </h3>
+            <h6 class="room-availability">{{ results[index].available_room_count }} of {{ results[index].total_room_count }} rooms available</h6>
+            <h6 class="date-available">Earliest availabe: {{ results[index].earliest_available_date }}</h6>
         </div>
     </div>
 	`
