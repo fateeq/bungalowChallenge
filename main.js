810994a3cc3d4bb04b46c0377ee989d6b2e7c092
fateeq <44147888+fateeq@users.noreply.github.com>
2019-03-28 16:33:58 -0400

    created card component

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
+Vue.component('card', {
+	props: {
+		results: []
+	},
+	template: `
+		<div id="card">
+        <img :src="results[0].images[0].sm_url"/>
+        <div class="listing-details">
+            <div class="room-price">$ {{ results[0].room_prices[0] }}</div>
+            <h3 class="headline"> {{ results[0].headline }} </h3>
+            <h6 class="room-availability">{{ results[0].available_room_count }} of {{ results[0].total_room_count }} rooms available</h6>
+            <h6 class="date-available">Earliest availabe: {{ results[0].earliest_available_date }}</h6>
+        </div>
+    </div>
+	`
+})
+
 var app = new Vue({
 	el: '#app',
-	data: {
-		results: [],
-		id: 16,
-		image: 'test.jpg',
-		roomPrice: 916,
-		headline: 'Historic Home in Queen Anne',
-		roomAvailability: 1,
-		totalRoomCount: 5,
-		earliestAvailable: '2019-03-28',
+	data() {
+		return {
+			results: [],
+		}
 	},
 	mounted: function() {
 		var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
