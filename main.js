436c3cf6800dcfd2a3cdc632ac8ca43bcd79d9af
fateeq <44147888+fateeq@users.noreply.github.com>
2019-03-28 14:44:55 -0400

    replaced hard-coded data with API data

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
 	el: '#app',
 	data: {
+		results: [],
 		id: 16,
 		image: 'test.jpg',
 		roomPrice: 916,
@@ -8,5 +9,19 @@ var app = new Vue({
 		roomAvailability: 1,
 		totalRoomCount: 5,
 		earliestAvailable: '2019-03-28',
+	},
+	mounted: function() {
+		var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+		var targetUrl = 'https://stage-fieldstone.bungalow.com/api/v1/listings/properties/?market__slug=seattle'
+		fetch(proxyUrl+targetUrl)
+		.then( (response) => {
+			return response.json()
+		})
+		.then((jsonData) => {
+			this.results = jsonData.results
+		})
+		.catch(function(error) {  
+		  console.log('Request failed', error)  
+		});
 	}
 })
