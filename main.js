2c5f6fec736892f9be76ac74389fd477d78a4709
fateeq <44147888+fateeq@users.noreply.github.com>
2019-03-29 14:11:20 -0400

    added searchbox to filter through listings headlines

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
 Vue.component('card', {
 	props: {
-		results: [],
-		index: ''
+		results: '',
+		index: '',
 	},
 	template: `
 		<div class="card">
         <div class="image-container">
-        	<img :src="results[index].images[0].sm_url"/>
-        	<div class="room-price" v-if="results[index].available_room_count === 1">$ {{ results[index].room_prices[0] }}</div>
-        	<div class="room-price" v-else>$ {{ Math.min(...results[index].room_prices) }} +</div>
+        	<img :src="listing.images[0].sm_url"/>
+        	<div class="room-price" v-if="listing.available_room_count === 1">$ {{ listing.room_prices[0] }}</div>
+        	<div class="room-price" v-else>$ {{ Math.min(...listing.room_prices) }} +</div>
         </div>
         <div class="listing-details">
-            <h3 class="headline"> {{ results[index].headline }} </h3>
-            <h6 class="room-availability">{{ results[index].available_room_count }} of {{ results[index].total_room_count }} rooms available</h6>
-            <h6 class="date-available">Earliest availabe: {{ results[index].earliest_available_date }}</h6>
+            <h3 class="headline"> {{ listing.headline }} </h3>
+            <h6 class="room-availability">{{ listing.available_room_count }} of {{ listing.total_room_count }} rooms available</h6>
+            <h6 class="date-available">Earliest availabe: {{ listing.earliest_available_date }}</h6>
         </div>
     </div>
-	`
+	`,
+	computed: {
+		listing() {
+			return this.results[this.index]
+		}
+	}
 })
 
 var app = new Vue({
 	el: '#app',
 	data() {
 		return {
-			results: []
+			results: [],
+			search: ''
+		}
+	},
+	computed: {
+		filteredResults: function() {
+			return this.results.filter((listing) => {
+				return listing.headline.toLowerCase().includes(this.search.toLowerCase())
+			})
 		}
 	},
 	mounted: function() {
