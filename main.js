9d5ec91a42a284c4300f0cf359969638a84e7449
fateeq <44147888+fateeq@users.noreply.github.com>
2019-03-28 21:18:47 -0400

    updated styling

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ Vue.component('card', {
 	},
 	template: `
 		<div id="card">
-        <img :src="results[index].images[0].sm_url"/>
+        <div id="image-container">
+        	<img :src="results[index].images[0].sm_url"/>
+        	<div class="room-price">$ {{ results[index].room_prices[0] }}</div>
+        </div>
         <div class="listing-details">
-            <div class="room-price">$ {{ results[index].room_prices[0] }}</div>
             <h3 class="headline"> {{ results[index].headline }} </h3>
             <h6 class="room-availability">{{ results[index].available_room_count }} of {{ results[index].total_room_count }} rooms available</h6>
             <h6 class="date-available">Earliest availabe: {{ results[index].earliest_available_date }}</h6>
